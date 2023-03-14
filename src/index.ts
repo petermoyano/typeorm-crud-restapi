f8a4bf40e50a906c66a886d350336357aee01571
Peter M <pmoyano@solvd.com>
2023-03-14 09:41:03 -0300

    basic installs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import "reflect-metadata";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
